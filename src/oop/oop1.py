c828e71f664378a2883607fd60171e1f19b33e77
Caleb <bargercj20@hotmail.com>
2020-07-10 08:04:47 -0700

    chow

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,24 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+class Vehicle: # <--- PARENT CLASS HERE
+    pass
+
+class FlightVehicle(Vehicle):
+    pass
+
+class Starship(FlightVehicle):
+    pass
+
+class Airplane(FlightVehicle):
+    pass
+
+class GroundVehicle(Vehicle):
+    pass
+
+class Car(GroundVehicle):
+    pass
+
+class Motercycle(GroundVehicle):
+    pass
