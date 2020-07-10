2082fa1f5663e1ab2105d37c49829ca2f2fe82c4
Caleb <bargercj20@hotmail.com>
2020-07-10 08:48:00 -0700

    hit mvp

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,5 +1,16 @@
+from csv import reader
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
+
+class City:
+  # indexs 0, 3, 4
+  def __init__ (self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
+
+  def __str__(self):
+    return f"{self.name} - {self.lat}, {self.lon}"
 
 
 # We have a collection of US cities with population over 750,000 stored in the
@@ -20,8 +31,15 @@ def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
-    return cities
+
+  with open('cities.csv', 'r') as read_obj:
+    csv_reader = reader(read_obj)
+  
+    for row in csv_reader:
+      if not row[0] == "city":
+        city = City(row[0], row[3], row[4])
+        cities.append(city)
+  return cities
 
 cityreader(cities)
 
